9e092a6dd554ac4cf0985ede9e810166859f2a42
NickWaddington <nick.waddington@zonedigital.com>
2023-06-21 16:04:10 +0100

    cartscreen

---
+++ frontend/src/screens/CartScreen.jsx
@@ -0,0 +1,23 @@
+import { Link, useNavigate } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
+import {
+  Row,
+  Col,
+  ListGroup,
+  Image,
+  Form,
+  Button,
+  Card,
+} from 'react-bootstrap';
+import { FaTrash } from 'react-icons/fa';
+import Message from '../components/Message';
+
+const CartScreen = () => {
+  const navigate = useNavigate();
+
+  const dispatch = useDispatch();
+
+  return <div>cartScreen</div>;
+};
+
+export default CartScreen;
