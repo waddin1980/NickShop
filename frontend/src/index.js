67fd52f7041b7732aa725ffa9ab34c642cd9ee5d
NickWaddington <nick.waddington@zonedigital.com>
2023-06-26 16:59:50 +0100

    added shipping form

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -15,6 +15,7 @@ import './index.css';
 
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import PrivateRoute from './components/PrivateRoute';
 
 // Pages
 import HomeScreen from './screens/HomeScreen';
@@ -22,6 +23,7 @@ import ProductScreen from './screens/ProductScreen';
 import CartScreen from './screens/CartScreen';
 import LoginScreen from './screens/LoginScreen';
 import RegisterScreen from './screens/RegisterScreen';
+import ShippingScreen from './screens/ShippingScreen';
 
 const router = createBrowserRouter(
   createRoutesFromElements(
@@ -31,6 +33,10 @@ const router = createBrowserRouter(
       <Route path="/cart" element={<CartScreen />} />
       <Route path="/login" element={<LoginScreen />} />
       <Route path="/register" element={<RegisterScreen />} />
+
+      <Route path="" element={<PrivateRoute />}>
+        <Route path="/shipping" element={<ShippingScreen />} />
+      </Route>
     </Route>
   )
 );
